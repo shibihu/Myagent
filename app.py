5afdaca7afa35f45f1a45cb1c34b9a954d55405f
shibihu <shib9895@gmail.com>
2026-07-18 06:11:37 +0700

    update

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
 
 app = FastAPI(title="MyAgent")
 
-# Static Files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# HTML Templates
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html",
+        context={"request": request},
     )
